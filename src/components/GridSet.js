ce725c06456cbc09d35e95ba194530cf656e5c65
JeffRisberg <jrisberg@aol.com>
2019-01-28 22:14:37 -0800

    filled in missing imports

--- src/components/GridSet.js
+++ src/components/GridSet.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import {withStyles} from 'material-ui/styles';
-import Footer from "./Footer";
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 const GridSet = () => (
 <div className={classes.root}>
