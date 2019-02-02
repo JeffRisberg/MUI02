eaf976aeb4d8c1fc602a3496a412302fd6dd02ca
JeffRisberg <jrisberg@aol.com>
2019-02-01 23:10:23 -0800

    added withStyles to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import FilterLink from '../containers/FilterLink';
+import {withStyles} from '@material-ui/core/styles';
 
-const Footer = () => (
-  <p>
-    <FilterLink filter="SHOW_ALL">All</FilterLink>
-    <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>
-    <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
-  </p>
-);
+const styles = {
+    root: {
+        marginTop: "10px",
+        background: "#f44"
+    },
+};
 
-export default Footer;
+function Footer(props) {
+    const {classes} = props;
+    const spacing = 16;
+
+    return (
+        <p className={classes.root}>
+            <FilterLink filter="SHOW_ALL">All</FilterLink>
+            <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>
+            <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+        </p>
+    );
+}
+
+export default withStyles(styles)(Footer);
