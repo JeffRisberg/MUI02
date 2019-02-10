0473adda80c13602677c540d3d2cc9d4a2ad64ff
JeffRisberg <jrisberg@aol.com>
2019-02-09 21:14:10 -0800

    merged in layouts from RE03

--- src/AppRoot.js
+++ src/AppRoot.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import Footer from './Footer';
-import AddTodo from '../containers/AddTodo';
-import VisibleTodoList from '../containers/VisibleTodoList';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/VisibleTodoList';
 import Card from 'material-ui/Card';
 
 import { withStyles } from 'material-ui/styles';
