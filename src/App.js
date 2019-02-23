1e1b1a85251c74f88ad39b24102607bf403953ea
JeffRisberg <jrisberg@aol.com>
2019-02-23 14:13:06 -0800

    updated to match more parts of MUI01 and WH01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+import Paper from '@material-ui/core/Paper';
 import Footer from './components/Footer';
-
 import Splash from './views/Splash'
 import Items from './views/Items';
 
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 import {withStyles} from '@material-ui/core/styles';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import grey from '@material-ui/core/colors/grey';
 
 const styles = {
     root: {
@@ -24,19 +30,35 @@ const styles = {
     },
 };
 
+const theme = createMuiTheme({
+    palette: {
+        primary: lightBlue,
+        secondary: grey,
+        text: {
+            primary: '#000',
+            secondary: '#888'
+        }
+    },
+    status: {
+        danger: 'orange',
+    },
+});
+
 class App extends Component {
 
     render() {
         return (
-            <div className={props.classes.root}>
-                <Router>
-                    <Switch>
-                        <Route path="/" exact component={Splash}/>
-                        <Route path="/items" component={Items}/>
-                    </Switch>
-                </Router>
-                <Footer/>
-            </div>
+            <MuiThemeProvider theme={theme}>
+                <Paper className={classes.app}>
+                    <Router>
+                        <Switch>
+                            <Route path="/" exact component={Splash}/>
+                            <Route path="/items" component={Items}/>
+                        </Switch>
+                    </Router>
+                    <Footer/>
+                </Paper>
+            </MuiThemeProvider>
         )
     }
 }
