9d0e0639b003d5f4d58087755c08be99159ee1e6
JeffRisberg <jrisberg@aol.com>
2019-03-14 18:40:37 -0700

    first example of using Tables in MaterialUI

--- src/components/Items/ItemListComponent.js
+++ src/components/Items/ItemListComponent.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+import {Link} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = {
@@ -12,66 +18,66 @@ const styles = {
 };
 
 class ItemListComponent extends Component {
-  static propTypes = {
-    records: PropTypes.array.isRequired,
-    status: PropTypes.object.isRequired,
-    toggleItem: PropTypes.func.isRequired,
-  };
+   static propTypes = {
+      records: PropTypes.array.isRequired,
+      status: PropTypes.object.isRequired,
+      toggleItem: PropTypes.func.isRequired,
+   };
 
-  formatEpochTime(epochTime) {
-    const date = new Date(Number(epochTime) * 1000);
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-  }
+   static formatEpochTime(epochTime) {
+      const date = new Date(Number(epochTime) * 1000);
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+   }
 
-  render() {
-    const { records, status } = this.props;
+   render() {
+      const {records, status} = this.props;
 
-    if (status.isFetching) {
+      if (status.isFetching) {
+         return (
+            <div className="items__list">
+               Loading
+            </div>
+         );
+      }
+
+      const itemNodes = records.map((item, key) => {
+         const id = item.id;
+         const valueStr = item.value.toLocaleString(undefined, {minimumFractionDigits: 2});
+
+         return (
+            <TableRow key={key}>
+               <TableCell><Link to={'/items/detail/' + id} className='btn btn-default'>View</Link></TableCell>
+               <TableCell style={{textDecoration: item.completed ? 'line-through' : 'none'}}
+                   onClick={() => this.props.toggleItem(item)}>
+                  {item.name}
+               </TableCell>
+               <TableCell align='right'>${valueStr}</TableCell>
+               <TableCell>{item.description}</TableCell>
+               <TableCell>{this.formatEpochTime(item.lastUpdated)}</TableCell>
+            </TableRow>
+         );
+      });
+
       return (
-        <div className="items__list">
-          Loading
-        </div>
+         <Paper>
+            <Table>
+               <TableHead>
+                  <TableRow>
+                     <TableCell>{''}</TableCell>
+                     <TableCell>Name</TableCell>
+                     <TableCell align='right'>Value</TableCell>
+                     <TableCell>Description</TableCell>
+                     <TableCell>Last Updated</TableCell>
+                  </TableRow>
+               </TableHead>
+               <TableBody>
+                  {itemNodes}
+               </TableBody>
+            </Table>
+         </Paper>
       );
-    }
-
-    const itemNodes = records.map((item, key) => {
-      const id = item.id;
-      const valueStr = item.value.toLocaleString(undefined, { minimumFractionDigits: 2 });
-
-      return (
-        <tr key={key}>
-          <td><Link to={'/items/detail/' + id} className='btn btn-default'>View</Link></td>
-          <td style={{ textDecoration: item.completed ? 'line-through' : 'none' }}
-            onClick={() => this.props.toggleItem(item)}>
-            {item.name}
-          </td>
-          <td className="text-right">${valueStr}</td>
-          <td>{item.description}</td>
-          <td>{this.formatEpochTime(item.lastUpdated)}</td>
-        </tr>
-      );
-    });
-
-    return (
-      <div className="items__list">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>{''}</th>
-              <th>Name</th>
-              <th className="text-right">Value</th>
-              <th>Description</th>
-              <th>Last Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemNodes}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+   }
 }
 
 export default withStyles(styles)(ItemListComponent);
