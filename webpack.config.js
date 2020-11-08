5274b784c8c298b44b702bea0faf66e202c7cb98
JeffRisberg <j.s.risberg@gmail.com>
2020-11-07 22:01:10 -0800

    updated to current MiniCSSWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    javascript: "./src/index.js"
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader" ]
-      },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]" }
+    mode: 'development',
+    entry: {
+        javascript: "./src/index.js"
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=/public/icons/[name].[ext]"}
+        ]
+    },
+    plugins: [
+        new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
+        new MiniCssExtractPlugin({filename: 'public/style.css'})
     ]
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
-    new MiniCssExtractPlugin({ filename: 'public/style.css', allChunks: true })
-  ]
 };
